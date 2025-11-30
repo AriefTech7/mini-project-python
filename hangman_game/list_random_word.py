0a088f057b029f156a907fb67d637005c01bb48b
arif wahyudi nasution <arifwahyudinasution@gmail.com>
2025-11-30 15:38:54 +0700

    projct game hangman form udemy

---
+++ hangman_game/list_random_word.py
@@ -0,0 +1,27 @@
+word = [
+    # Kata benda
+    'komputer', 'laptop', 'mouse', 'keyboard', 'monitor',
+    'matahari', 'bulan', 'bintang', 'langit', 'awan',
+    'pohon', 'daun', 'bunga', 'rumah', 'kursi',
+
+    # Bahasa Pemrograman & Teknologi
+    'python', 'javascript', 'java', 'php', 'html',
+    'css', 'sql', 'cplusplus', 'ruby', 'swift',
+    'github', 'server', 'database', 'cloud', 'linux',
+
+    # Hewan
+    'kucing', 'anjing', 'kuda', 'gajah', 'harimau',
+    'singa', 'monyet', 'burung', 'ikan', 'ular',
+    'kangguru', 'zebra', 'jerapah', 'beruang', 'panda',
+
+    # Makanan & Minuman
+    'nasi', 'ayam', 'mie', 'sate', 'rendang',
+    'es teh', 'jus', 'kopi', 'roti', 'kue',
+    'soto', 'bakso', 'gado gado', 'sushi', 'pizza',
+
+    # Aktivitas/Kata Kerja
+    'belajar', 'kerja', 'tidur', 'jalan', 'lari',
+    'coding', 'menulis', 'membaca', 'bermain', 'berenang',
+
+    'aardvark', 'baboon', 'camel'
+]
